f05f9294e399989bce3f468576104e521e23bfd8
moba1 <moba2@protonmail.com>
2024-01-06 02:13:13 +0900

    implement From & Into to Id

--- src/format/header/id.rs
+++ src/format/header/id.rs
@@ -1,9 +1,15 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Id(pub u16);
 
-impl Id {
-    pub fn to_hex_string(&self) -> String {
-        format!("{:04X}", self.0)
+impl std::convert::Into<u16> for Id {
+    fn into(self) -> u16 {
+        self.0
+    }
+}
+
+impl std::convert::From<u16> for Id {
+    fn from(value: u16) -> Self {
+        Self(value)
     }
 }
 
@@ -13,8 +19,13 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn it_converts_to_hex_string() {
-        assert_eq!(Id(0x0000_u16).to_hex_string(), "0000");
-        assert_eq!(Id(0xffff_u16).to_hex_string(), "FFFF");
+    fn it_converts_from_u16_value() {
+        assert_eq!(Id::from(0xABCD), Id(0xABCD));
+    }
+
+    #[test]
+    fn it_converts_to_u16_value() {
+        let value: u16 = Id(0xABCD).into();
+        assert_eq!(value, 0xABCD_u16);
     }
 }
